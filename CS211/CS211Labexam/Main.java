c0d876edb16ba397b8c415764de462398e7816bc
Emily <113935577+WeiEmily@users.noreply.github.com>
2023-03-21 09:11:30 +0000

    add entire class

--- CS211/CS211Labexam/Main.java
+++ CS211/CS211Labexam/Main.java
@@ -1,4 +1,9 @@
 package CS211Labexam;
 
+import CS211Lab5.LinkedBinaryTree;
+
 public class Main {
+    public static void main(String[] args) {
+        CS211Lab5.LinkedBinaryTree<String> bsTree = new LinkedBinaryTree<>();
+    }
 }
